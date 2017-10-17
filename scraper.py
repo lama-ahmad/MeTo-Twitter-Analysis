91b01892138c4c89cd2ef5ce4705127bf506a581
Lama Ahmad <lamaahmad@172-16-54-246.DYNAPOOL.NYU.EDU>
2017-10-17 14:52:48 -0400

    working scraper

--- scraper.py
+++ scraper.py
@@ -3,17 +3,16 @@ import csv
 import config
 import pandas as pd
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
-#####United Airlines
 # Open/Create a file to append data
-csvFile = open('ua.csv', 'a')
+csvFile = open('metoo.csv', 'a')
 #Use csv Writer
 csvWriter = csv.writer(csvFile)
 
-for tweet in tweepy.Cursor(api.search,q="#unitedAIRLINES",count=100,
+for tweet in tweepy.Cursor(api.search,q="#MeToo",count=100,
                            lang="en",
-                           since="2017-04-03").items():
+                           since="2017-10-15").items():
     print (tweet.created_at, tweet.text)
     csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
